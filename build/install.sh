8fa0dc92fe65c5a4659034a27c26b266a6860c6e
rossedlin <rossedlin@gmail.com>
2017-04-17 19:10:08 +0100

    update install file

--- build/install.sh
+++ build/install.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-./clean.sh
+echo "Install -> www.rossedlin.co.uk"
+cd ~
 
-cd ..
-composer install
+#Cleanup
+rm -R -f www.rossedlin.co.uk
+
+#Git
+git clone https://github.com/rossedlin/www.rossedlin.co.uk
+
+#Composer
+cd www.rossedlin.co.uk
+/usr/bin/php-5.6 composer.phar install
+
+#Envronment File
+cp public_html/.env www.rossedlin.co.uk/.env
+
+#Folders
+cd ~
+mv public_html old_html
+mv www.rossedlin.co.uk public_html
+rm -R -f old_html
+
+#Symlink Install File
+cd ~
+rm install.sh
+ln public_html/build/install.sh install.sh
+chmod +x install.sh
