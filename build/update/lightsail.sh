041cb4c982f248138eb45ea076c32b1fafd52058
Ross Edlin <rossedlin@gmail.com>
2017-05-08 16:55:58 +0100

    lightsail hi

---
+++ build/update/lightsail.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "hi"
+
+#cd ~
+#mv public_html old_html
+#mv www.rossedlin.co.uk public_html
+#rm -R -f old_html
+
+#Symlink Install File
+#cd ~
+#rm install.sh
+#ln public_html/www_rossedlin/build/install.sh install.sh
+#chmod +x install.sh
